50ee6a54af21e5bf6663a6dbacb605afad3317f3
luiscarlosfr <luiscarlosfrancomora@gmail.com>
2023-07-20 13:29:48 -0500

    tercer commit

--- Controllers/main.js
+++ Controllers/main.js
@@ -1,9 +1,11 @@
-const btnLogin = document.getElementById("btn-login")
+const btnlogin = document.getElementById("btn-login")
 const emailinput = document.getElementById("email-input")
 const passwordinput = document.getElementById("pass-input")
 
-btnLogin.addEventListener("click", function () {
+btnlogin.addEventListener("click", function (event) {
+    event.preventDefault
     let email = emailinput.value;
     let password = passwordinput.value;
-    console.log(email,password)
-})
+
+    window.location.href = "../home.html";
+});

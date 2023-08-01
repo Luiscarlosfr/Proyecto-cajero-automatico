b028c2425c813bbda141b7aba61f0487b4a8cfb0
luiscarlosfr <luiscarlosfrancomora@gmail.com>
2023-07-31 20:48:22 -0500

    nuevo commit

--- Controllers/main.js
+++ Controllers/main.js
@@ -1,12 +1,23 @@
-const btnlogin = document.getElementById("btn-login");
+
 const emailinput = document.getElementById("email-input");
 const passwordinput = document.getElementById("pass-input");
+const btnlogin = document.getElementById("btn-login");
+
+
+
 
 btnlogin.addEventListener("click", function (event) {
   event.preventDefault;
   let email = emailinput.value;
   let password = passwordinput.value;
-  console.log(email);
-  console.log(password);
+ // console.log(email);
+ // console.log(password);
   window.location.href = "./home.html";
 });
+
+let email="";
+emailinput.addEventListener("change",function(){email=emailinput.value
+console.log("emailusuario", emailinput)})
+let password="";
+passwordinput.addEventListener("change",function(){password=passwordinput.value
+  console.log("passwordusuario", passwordinput)})
